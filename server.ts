d1e73903f009ab7c5e38b37331b5d57dac62e9a7
Puentebravo <puentematos@gmail.com>
2022-10-08 15:43:18 -0400

    Linked routes to server

--- server.ts
+++ server.ts
@@ -3,10 +3,12 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.port || 3001;
 const app = express();
+const routes = require("./routes/apiRoutes")
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 
 
