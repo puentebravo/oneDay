d561762be136c632d798a19e89816af9aa7aab38
Puentebravo <puentematos@gmail.com>
2022-10-10 22:36:30 -0400

    Added get, post routes, serverside form validation

--- routes/apiRoutes.ts
+++ routes/apiRoutes.ts
@@ -1,6 +1,39 @@
-const { PrismaClient } = require("@prisma/client")
+import { PrismaClient } from "@prisma/client";
+import { request, Request, Response } from "express";
+import { body, validationResult } from "express-validator"
 const router = require("express").Router();
 
 
 const prisma = new PrismaClient({log: ['query', 'error']})
 
+
+
+router.get("api/getSaved", async (req: Request, res: Response) => {
+
+    
+    const saved = await prisma.savedDates.findMany({})
+
+    res.json(saved)
+})
+
+router.post("api/saveDate", body(["title", "high", "low", "weather", "story", "photoSrc"]).isAlphanumeric(), body("date").isDate(), async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()})
+    }
+
+    const data = prisma.savedDates.create({
+        data: {
+            title: req.body.title,
+            date: req.body.date,
+            high: req.body.high,
+            low: req.body.low,
+            weather: req.body.weather,
+            story: req.body.story,
+            photoSrc: req.body.photoSrc
+        }
+    })
+
+    res.json(data)
+})
