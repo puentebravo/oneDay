846ab40ede15bf16f20ddb7c1cfe228cb6a65fb2
Puentebravo <puentematos@gmail.com>
2022-10-11 21:41:16 -0400

    replaced isAlphanumeric() validator with isAscii()

--- routes/apiRoutes.ts
+++ routes/apiRoutes.ts
@@ -1,39 +1,49 @@
 import { PrismaClient } from "@prisma/client";
-import { request, Request, Response } from "express";
-import { body, validationResult } from "express-validator"
+import { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 const router = require("express").Router();
 
+const prisma = new PrismaClient({ log: ["query", "error"] });
 
-const prisma = new PrismaClient({log: ['query', 'error']})
+router.get("/api/getSaved", async (req: Request, res: Response) => {
+  const saved = await prisma.savedDates.findMany({});
 
+  res.json(saved);
+});
 
-
-router.get("api/getSaved", async (req: Request, res: Response) => {
-
-    
-    const saved = await prisma.savedDates.findMany({})
-
-    res.json(saved)
-})
-
-router.post("api/saveDate", body(["title", "high", "low", "weather", "story", "photoSrc"]).isAlphanumeric(), body("date").isDate(), async (req: Request, res: Response) => {
+router.post(
+  "/api/saveDate",
+  body([
+    "title",
+    "date",
+    "high",
+    "low",
+    "weather",
+    "story",
+    "photoSrc",
+  ]).isAscii(),
+  body("date").isDate(),
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array()})
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const data = prisma.savedDates.create({
-        data: {
-            title: req.body.title,
-            date: req.body.date,
-            high: req.body.high,
-            low: req.body.low,
-            weather: req.body.weather,
-            story: req.body.story,
-            photoSrc: req.body.photoSrc
-        }
-    })
+    const data = await prisma.savedDates.create({
+      data: {
+        title: req.body.title,
+        date: req.body.date,
+        high: req.body.high,
+        low: req.body.low,
+        weather: req.body.weather,
+        story: req.body.story,
+        photoSrc: req.body.photoSrc,
+      },
+    });
 
-    res.json(data)
-})
+    res.json(data);
+  }
+);
+
+module.exports = router
