1bbc2feca2d56e90a9e02b8b0ef5f67c330cd1c9
Puentebravo <puentematos@gmail.com>
2022-11-10 20:44:48 -0500

    added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home';
+import Saved from './pages/saved';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router basename={`${process.env.PUBLIC_URL}`}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dates" element={<Saved />} />
+      </Routes>
+    </Router>
+  )
+
 }
 
 export default App;
