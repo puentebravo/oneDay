0de2f35e4a67298e1abe423245889e8049917c3b
Puentebravo <puentematos@gmail.com>
2022-11-10 20:42:55 -0500

    Wrote navbar, linked to pages

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,2 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+function Navbar() {
+    return (
+        <nav>
+            <h1 className="font-neon glow text-left">One Day</h1>
+            <ul id="navList">
+                <li className="navLink"><Link to={"/"}>Home</Link></li>
+                <li className="navLink inactive"><Link to={"/dates"}>History</Link></li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Navbar;
