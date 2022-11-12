002f491df6d4606ad45c081f7c01f0c509cf30ca
Charlie Puente <puentematos@gmail.com>
2022-11-12 16:01:58 -0500

    Scaffolded out searchbar component

--- client/src/components/searchBar.tsx
+++ client/src/components/searchBar.tsx
@@ -1,1 +1,22 @@
 import React from "react";
+
+interface city {
+    city: string
+    setCity: (params: any) => any
+}
+
+function SearchBar(props: city) {
+    return (
+        <form>
+            <label htmlFor="input">
+                <input 
+                type="text"
+                value={props.city}
+                onChange={(e) => props.setCity(e.target.value)}
+                />
+            </label>   
+        </form>
+    )
+}
+
+export default SearchBar;
