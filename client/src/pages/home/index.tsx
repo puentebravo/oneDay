26fb0ff5878843dae3bb2916b711f4f0816011f9
Charlie Puente <puentematos@gmail.com>
2022-11-12 16:02:38 -0500

    Added weathercontainer to homepage

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Navbar from "../../components/navbar";
+import WeatherContainer from "../../components/weatherContainer";
+
 
 function Home() {
     return (
+        <>
         <Navbar />
+        <WeatherContainer />
+        </>
     )
 }
 
