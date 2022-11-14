0b9dcf452c6fa3d8a8e3012865b3f9620c7ac738
Charlie Puente <puentematos@gmail.com>
2022-11-13 20:17:17 -0500

    Added submit button to searchbar, fixed type error in props

--- client/src/components/searchBar.tsx
+++ client/src/components/searchBar.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 
 interface city {
-    city: string
-    setCity: (params: any) => any
+  city: string | undefined;
+  setCity: (params: any) => any;
 }
 
 function SearchBar(props: city) {
-    return (
-        <form>
-            <label htmlFor="input">
-                <input 
-                type="text"
-                value={props.city}
-                onChange={(e) => props.setCity(e.target.value)}
-                />
-            </label>   
-        </form>
-    )
+  return (
+    <section id="searchBar">
+      <form>
+        <label htmlFor="citySearch">
+          <input
+            id="citySearch"
+            type="text"
+            placeholder="Search"
+            value={props.city}
+            onChange={(e) => props.setCity(e.target.value)}
+          />
+        </label>
+        <button className="button">Search</button>
+      </form>
+    </section>
+  );
 }
 
 export default SearchBar;
