91d5fb509be980325c1ce692785aaf133989147d
Puentebravo <puentematos@gmail.com>
2022-11-15 20:52:04 -0500

    cleaned up CSS, added useEffect hook to homepage

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from "../../components/navbar";
 import SearchBar from "../../components/searchBar";
 import "./home.css"
@@ -6,7 +6,48 @@ import "./home.css"
 
 function Home() {
 
+    interface Coords {
+        lat: number,
+        lon: number
+    }
+
+    const [coords, setCoords] = useState({lon: 0, lat: 0})
+
     const [city, setCity] = useState()
+
+    const [weatherData, setWeatherData] = useState({})
+
+
+
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setCoords({lon: position.coords.longitude, lat: position.coords.latitude})
+            })
+        } else {
+            setCoords({lon: 0, lat: 0})
+        }
+
+        async function fetchWeather(coordinates:Coords) {
+            const data = await fetch(`/api/getLocalWeather/${coordinates.lat}/${coordinates.lon}`)
+
+            const jsonData = await data.json()
+
+            setWeatherData(jsonData)
+
+            if (jsonData) {
+                console.log(weatherData)
+            } else {
+                console.log("Loading")
+            }
+            
+        }
+
+        fetchWeather(coords)
+
+        
+
+    }, [])
 
 
     return (
