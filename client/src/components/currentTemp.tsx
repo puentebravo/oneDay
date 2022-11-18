802d585557eba13213fc6663d87e9d533411ddbe
Puentebravo <puentematos@gmail.com>
2022-11-18 14:37:41 -0500

    Scaffolded out current temps component

--- client/src/components/currentTemp.tsx
+++ client/src/components/currentTemp.tsx
@@ -1,1 +1,22 @@
 import React from "react";
+
+
+function CurrentTemp() {
+    return (
+        <section id="cTemp">
+            <div id="tempHeader">
+                <h2> Temperature: 43F</h2>
+                <img src="" alt="icon representing today's weather" id="weatherIcon" />
+            </div>
+            <div id="weatherList">
+                <ul>
+                    <li>Wind Speed</li>
+                    <li>Humidity</li>
+                </ul>
+            </div>
+
+        </section>
+    )
+}
+
+export default CurrentTemp;
