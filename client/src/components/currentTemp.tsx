ec5b5cd7a4ac0b8356b6ebd6e6a8f0af0890430f
Puentebravo <puentematos@gmail.com>
2022-11-22 21:15:15 -0500

    Added props to component

--- client/src/components/currentTemp.tsx
+++ client/src/components/currentTemp.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 
+interface WeatherObject {
+    current: number,
+    icon: string,
+    sunrise: number,
+    sunset: number,
+    temp: number,
+    feelsLike: number,
+    windSpeed: number,
+    UVI: number,
+    humidity: number
+}
 
-function CurrentTemp() {
+function CurrentTemp(props: WeatherObject) {
+
+    
+    const sunriseTime = new Date(props.sunrise)
+    const sunsetTime = new Date(props.sunset)
+    
+
+    
+
     return (
         <section id="cTemp">
             <div id="tempHeader">
-                <h2> Temperature: 43F</h2>
-                <img src="" alt="icon representing today's weather" id="weatherIcon" />
+                <h2> Temperature: {props.current}</h2>
+                <img src={props.icon} alt="icon representing today's weather" id="weatherIcon" />
             </div>
             <div id="weatherList">
                 <ul>
-                    <li>Wind Speed</li>
-                    <li>Humidity</li>
+                    <li>Feels Like: {props.feelsLike}</li>
+                    <li>Wind Speed: {props.windSpeed}</li>
+                    <li>Sunrise: {sunriseTime.toLocaleTimeString("en-US")} </li>
+                    <li>Sunset: {sunsetTime.toLocaleTimeString("en-US")}</li>
+                    <li>UV Index: {props.UVI}</li>
+                    <li>Humidity: {props.UVI}</li>
                 </ul>
             </div>
 
