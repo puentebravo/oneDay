b6905fd04572483f36d0286e49b7792a8fd1c531
Puentebravo <puentematos@gmail.com>
2022-12-01 19:46:41 -0500

    Removed sunrise/sunset parameters

--- client/src/components/currentTemp.tsx
+++ client/src/components/currentTemp.tsx
@@ -16,8 +16,7 @@ interface WeatherObject {
 function CurrentTemp(props: WeatherObject) {
 
     
-    // const sunriseTime = new Date(props.sunrise)
-    // const sunsetTime = new Date(props.sunset)
+
     const imgUrl = `http://openweathermap.org/img/wn/${props.icon}@2x.png`
     let tempUnit;
     let speedUnit;
@@ -39,15 +38,13 @@ function CurrentTemp(props: WeatherObject) {
     return (
         <section id="cTemp" className="card">
             <div id="tempHeader">
-                <h2> Temperature: {props.current}° {tempUnit}</h2>
+                <h2 className="font-space"> Temperature: {props.current}° {tempUnit}</h2>
                 <img src={imgUrl} alt="icon representing today's weather" id="weatherIcon" />
             </div>
             <div id="weatherList">
-                <ul>
+                <ul className="font-space">
                     <li>Feels Like: {props.feelsLike}° {tempUnit}</li>
                     <li>Wind Speed: {props.windSpeed} {speedUnit}</li>
-                    {/* <li>Sunrise: {sunriseTime.toLocaleTimeString("en-US", {hour12: false, timeStyle: "short"})} </li>
-                    <li>Sunset: {sunsetTime.toLocaleTimeString("en-US", {hour12: false, timeStyle: "short"})}</li> */}
                     <li>UV Index: {props.UVI}</li>
                     <li>Humidity: {props.humidity}%</li>
                 </ul>
