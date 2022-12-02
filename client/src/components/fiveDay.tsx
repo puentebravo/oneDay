24a62ea6433f8a680263170f88a18f317d701323
Puentebravo <puentematos@gmail.com>
2022-12-01 22:03:46 -0500

    Scaffolded five day forecast component

--- client/src/components/fiveDay.tsx
+++ client/src/components/fiveDay.tsx
@@ -1,1 +1,23 @@
 import React from "react";
+import ForecastContainer from "./foreCastCard";
+
+
+function FiveDay() {
+
+
+    const arr = [1 , 2, 3, 4, 5]
+    return (
+        <section className="card font-space" id="fiveDay">
+            <div id="forecastContainer">
+
+                {arr.map( (element) => (
+                    <ForecastContainer key={element}/>
+                ))}
+
+            </div>
+        </section>
+
+    )
+}
+
+export default FiveDay;
