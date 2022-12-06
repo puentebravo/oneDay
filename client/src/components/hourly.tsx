4558129e3e2d96574b0a758d45d8d3f661d5b3a5
Puentebravo <puentematos@gmail.com>
2022-12-06 15:50:48 -0500

    Added hourly component, scaffolded structure

--- client/src/components/hourly.tsx
+++ client/src/components/hourly.tsx
@@ -1,1 +1,33 @@
 import React from "react";
+import { currentWeatherObj } from "../interfaces"
+
+interface hourlyForecasts {
+    hourly: currentWeatherObj[],
+    units: string
+}
+
+
+function Hourly(props: hourlyForecasts) {
+
+
+    const hours = props.hourly.map(element => {
+        return {
+            time: element.dt,
+            temp: element.temp,
+            feelsLike: element.feels_like,
+            weather: element.weather[0].description
+        }
+    })
+
+    return (
+        <section className="card font-space" id="hourly">
+            <div id="hourlyContainer">
+                {hours.map((element) => (
+                    <div>{element.temp}</div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Hourly;
