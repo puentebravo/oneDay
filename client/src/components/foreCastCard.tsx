c1c07a344b375611eb39a266fc0efb783ad1adcc
Puentebravo <puentematos@gmail.com>
2022-12-05 21:46:24 -0500

    added interface, imgUrl, dateTime translation, and props to forecast card

--- client/src/components/foreCastCard.tsx
+++ client/src/components/foreCastCard.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 
-interface Test {
-    key: number
+interface Forecast {
+    id: number,
+    date: number,
+    nighttime: number,
+    daytime: number,
+    weather: string,
+    icon: string
 }
 
-function ForecastContainer(props: Test) {
+function ForecastCard(props: Forecast) {
 
-    // const imgUrl = `http://openweathermap.org/img/wn/${props.icon}@2x.png`
+    const imgUrl = `http://openweathermap.org/img/wn/${props.icon}@2x.png`
+    const dtString = new Date(props.date * 1000)
+    const humanString = dtString.toLocaleDateString()
+
+    console.log(humanString)
+
+
+    
+
+
     return (
-        <figure key={props.key} className="foreCastCard">
-            <img src="" alt="weather icon"/>
-            <h6>Day: Unknown</h6>
-            <p>Nighttime: Probably cold</p>
+        <figure className="foreCastCard" key={props.id}>
+            <img src={imgUrl} alt="weather icon"/>
+            <h6>Date: {humanString} </h6>
+            <p>Conditions: {props.weather} </p>
+            <p>Night: {props.nighttime}°</p>
+            <p>Day: {props.daytime}°</p>
         </figure>
 
         
     )
 }
 
-export default ForecastContainer
+export default ForecastCard
