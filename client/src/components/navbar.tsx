37cde0020422c8076fac2e639d1ac351fc94b839
Puentebravo <puentematos@gmail.com>
2022-12-06 15:51:04 -0500

    Changed navbar font

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav>
             <h1 className="font-neon glow text-left">One Day</h1>
-            <ul id="navList">
+            <ul id="navList" className="font-space">
                 <li><Link to={"/"}>Home</Link></li>
                 <li><Link to={"/dates"}>History</Link></li>
             </ul>
