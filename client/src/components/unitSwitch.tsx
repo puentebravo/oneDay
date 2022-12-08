e388c0263212400368423269c497c065a463e9b9
Puentebravo <puentematos@gmail.com>
2022-12-08 16:08:29 -0500

    fixed and added unit switch toggle

---
+++ client/src/components/unitSwitch.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface unitProp {
+    unit: string,
+    setUnit: (params: any) => any
+}
+
+function UnitSwitch(props: unitProp) {
+
+    const handleModeSwitch = () => {
+        if (props.unit === "metric") {
+            props.setUnit("imperial")
+        } else {
+            props.setUnit("metric")
+        }
+
+        console.log(props.unit)
+    }
+
+    return (
+        <figure id="unitToggler">
+            <p className="font-space">{props.unit.toUpperCase()}</p>
+            <label className="switch">
+                <input type="checkbox" onChange={handleModeSwitch}/>
+                <span className="slider"></span>
+            </label>
+        </figure>
+
+    )
+}
+
+export default UnitSwitch;
