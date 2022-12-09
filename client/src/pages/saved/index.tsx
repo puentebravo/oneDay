f78441e6fdb32e5162711acbe3056192dde5fbea
Puentebravo <puentematos@gmail.com>
2022-12-09 13:55:58 -0500

    Scaffolded out saved dates page

--- client/src/pages/saved/index.tsx
+++ client/src/pages/saved/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "../../components/navbar";
+import "./saved.css"
 
 
 function Saved() {
+
+    let testArr = [1, 2, 3, 4, 5, 6]
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <main id="savedList" className="font-space">
+                {testArr.map( element => (
+                    <div>This is a thing, among other things.</div>
+                ))}
+            </main>
+        </>
     )
 
 }
