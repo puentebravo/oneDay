386ea07af4038b240700fbf013c2577e45ed4625
Puentebravo <puentematos@gmail.com>
2022-12-14 13:57:46 -0500

    Added form elements, formik plugins

--- client/src/components/modal.tsx
+++ client/src/components/modal.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useFormik } from "formik";
 
 interface modalOpts {
     status: boolean,
     setStatus: (params: any) => any
 }
+
 
 function Modal(props: modalOpts) {
 
@@ -12,17 +14,39 @@ function Modal(props: modalOpts) {
     }
 
 
+    const formik = useFormik({
+        initialValues: {
+            title: "",
+            story: "",
+            photo: "",
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2))
+        }
+    })
+
+
     return (
         <section className={`modal ${props.status ? 'show' : ""}`} onClick={handleClose}>
             <div className="modal-content font-space" onClick={e => e.stopPropagation()}>
                 <div className="modal-header">
-                    <h3 className="modal-title">Test</h3>
+                    <h3 className="modal-title text-lead">INPUT_DATA</h3>
                 </div>
                 <div className="modal-body">
-                    <p>Form stuff goes here</p>
+                    <form onSubmit={formik.handleSubmit}>
+                        <label htmlFor="title">Title</label>
+                        <input className="textEntry" id="title" name="title" type="text" onChange={formik.handleChange} value={formik.values.title} />
+
+                        <label htmlFor="story">What's the Story?</label>
+                        <input className="textEntry" id="story" name="story" type="text" onChange={formik.handleChange} />
+
+                        <label htmlFor="photo">Upload a photo to capture your day</label>
+                        <input className="textEntry" id="photo" name="photo" type="file" capture="environment" accept="image/*" onChange={formik.handleChange} />
+                        <button className="button" type="submit">SUBMIT</button>
+                    </form>
                 </div>
                 <div className="modal-footer">
-                    <button className="button" onClick={handleClose}>Close</button>
+                    <button className="button" onClick={handleClose}>CLOSE</button>
                 </div>
 
             </div>
