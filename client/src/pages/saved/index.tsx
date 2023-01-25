8b61301c6bad7770a9161777c3fdcc83bee6f2af
Puentebravo <puentematos@gmail.com>
2023-01-24 19:51:49 -0500

    Added fetch request to server for saveData, added caching, useEffect

--- client/src/pages/saved/index.tsx
+++ client/src/pages/saved/index.tsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { SaveData } from "../../interfaces";
+import DateCard from "../../components/dateCard";
+import ProgressBar from "../../components/progressBar";
 import Navbar from "../../components/navbar";
 import "./saved.css"
 
 
 function Saved() {
 
-    let testArr = [1, 2, 3, 4, 5, 6]
+    const [saveData, setSaveData] = useState<[SaveData]>()
+
+    async function getSavedData(){
+        const fetchResponse = await fetch("/api/getSaved")
+
+        const fetchJson = await fetchResponse.json()
+
+        localStorage.setItem("saveData", JSON.stringify(fetchJson))
+
+        setSaveData(fetchJson)
+    }
+
+    useEffect(() => {
+        let cachedSaveData = localStorage.getItem("saveData")
+
+        if (cachedSaveData) {
+            setSaveData(JSON.parse(cachedSaveData))
+        }
+
+        getSavedData();
+
+    }, [])
+    
     return (
         <>
             <Navbar />
             <main id="savedList" className="font-space">
-                {testArr.map( element => (
-                    <div>This is a thing, among other things.</div>
-                ))}
+
+                {
+                    saveData ? 
+                    saveData.map( element => (
+                        <DateCard date={element.date} title={element.title} high={element.high} low={element.low} weather={element.weather} story={element.story} photoSrc={element.photoSrc}/>
+                    ))
+
+                    : <ProgressBar />
+                }
             </main>
         </>
     )
 
 }
 
-export default Saved
+export default Saved;
