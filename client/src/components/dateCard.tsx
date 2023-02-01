a0a803ed6d3a58beec458f5ceea26d227b7b7249
Puentebravo <puentematos@gmail.com>
2023-02-01 15:04:35 -0500

    Added modal, button component to datecard

--- client/src/components/dateCard.tsx
+++ client/src/components/dateCard.tsx
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { SaveData } from "../interfaces";
+import ViewButton from "./viewButton";
+import ViewModal from "./viewModal";
 
 
 function DateCard(props: SaveData) {
 
+    const [show, setShow] = useState<boolean>(false)
+
 
     return (
-
-        <section className="card" >
-            <div className="dCardHeader">
-                <h2 className="font-space text-lead">{props.date}</h2>
-            </div>
-            <div className="dCardBody">
-                <p>{props.title}</p>
-                <p>Conditions: {props.weather}</p>
-                <p>High: {props.high}</p>
-                <p>Low: {props.low}</p>
-            </div>
-            <div className="dCardFooter">
-                <button className="button">View Memory</button>
-            </div>
-        </section>
-
+        <>
+            <section className="card" >
+                <div className="dCardHeader">
+                    <h2 className="font-space text-lead">{props.date}</h2>
+                </div>
+                <div className="dCardBody">
+                    <p>{props.title}</p>
+                    <p>Conditions: {props.weather}</p>
+                    <p>High: {props.high}</p>
+                    <p>Low: {props.low}</p>
+                </div>
+                <div className="dCardFooter">
+                    <ViewButton setOpen={setShow} />
+                </div>
+            </section>
+            <ViewModal status={show} setStatus={setShow} saveData={props} />
+        </>
     )
 
 }
